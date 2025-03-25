6d0dded7b91bf10f8aa45e54dba1826f945f23b8
Sveta Konstantinova <svetikons@mail.ru>
2025-03-25 19:51:27 +0300

    Updates

--- components/Header/utils.ts
+++
@@ -1,9 +0,0 @@
-interface IPage {
-    title: string;
-    path: string;
-}
-
-export const pages: IPage[] = [
-    { title: 'Products', path: 'books' },
-    { title: 'Create product', path: 'create-product' },
-];
